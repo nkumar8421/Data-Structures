dbe23de05d9b63a1683bd9c386e4be3b5c73a92f
nkumar8421 <nkumar8421@gmail.com>
2017-04-23 15:56:07 +0530

    Add files via upload

---
+++ ConvertTreeToDoublyIist.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BST
+{
+    class Node
+    {
+        public object data;
+        public Node left;
+        public Node right;
+        public Node(object d)
+        {
+            data = d;
+            left = right = null;
+        }
+    }
+    class ConvertTreeToDoublyIist
+    {
+       
+    public static Node prevNode = null;
+    public static Node head = null;
+        public static void toDoublyList(Node root)
+        {
+          if (root == null)
+            return;
+          toDoublyList(root.left);
+          if (head == null)
+              head = root;
+          else
+          {
+              root.left = prevNode;
+              prevNode.right = root;
+          }
+          prevNode = root;
+          toDoublyList(root.right);
+        }
+        public static void printList(Node head)
+        {
+
+            Node temp = head;
+            while(temp!=null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.right;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Node root = new Node(6);
+            root.left = new Node(4);
+            root.right=new Node(10);
+
+            root.left.left=new Node(3);
+            root.left.right=new Node(5);
+
+            root.right.left = new Node(9);
+            root.right.right = new Node(12);
+
+            toDoublyList(root);
+
+            printList(head);
+            Console.ReadLine();
+        }
+    }
+}
