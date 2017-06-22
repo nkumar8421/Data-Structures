afcff8bcc712f8922ab789e54f0031402503ab6f
nkumar8421 <nkumar8421@gmail.com>
2017-06-22 17:52:03 +0530

    Update Delegates.cs

--- Delegates.cs
+++ Delegates.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace Algo
 {
     public delegate void DelSumOfNumbers(int target);
-    class MostRepeatedElementInArray
+    class DelegateExample
     {
 
         
